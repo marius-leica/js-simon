a544d6fa0e1307519cb82fee486608e916ab84d7
Marius L <mariusleica@gmail.com>
2022-05-31 19:07:04 +0200

    Last part

--- main.js
+++ main.js
@@ -2,13 +2,17 @@
 const htmlNumbers = document.getElementById("html-numbers");
 
 function randomNumbers() {
+  let fiveRandomNumbers = [];
+
   for (let i = 0; i < 5; i++) {
     let randomNumber = Math.floor(Math.random() * 100 + 1);
     htmlNumbers.innerHTML += `<li>${randomNumber}</li>`;
+    fiveRandomNumbers.push(randomNumber);
   }
+  return fiveRandomNumbers;
 }
-let fiveRandomNumbers = randomNumbers();
-console.log(fiveRandomNumbers);
+let fiveRandomNumbersArray = randomNumbers();
+console.log(fiveRandomNumbersArray);
 
 // - Avviare un timer di 30 secondi
 // - Dopo 30 secondi, nascondere i numeri.
@@ -24,10 +28,23 @@ setTimeout(promptUser, 3500);
 
 function promptUser() {
   let userNumbers = [];
+  let numeriIndovinati = [];
 
   for (let i = 0; i < 5; i++) {
     let userNumber = parseInt(prompt("Inserisci un numero"));
     userNumbers.push(userNumber);
     console.log(userNumbers);
   }
+  for (let i = 0; i < userNumbers.length; i++) {
+    for (let j = 0; j < fiveRandomNumbersArray.length; j++) {
+      if (userNumbers[i] === fiveRandomNumbersArray[j]) {
+        numeriIndovinati.push(userNumbers[i]);
+        console.log(`Hai indovinato il numero ${userNumbers[i]}`);
+      }
+    }
+  }
+  console.log("Hai indovinato questi numeri:" + numeriIndovinati);
+  return userNumbers;
 }
+
+//- Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
